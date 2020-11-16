ba2d5526c15dd0a64e4254fec28859667405b9b0
enoki1 <74561881+enoki1@users.noreply.github.com>
2020-11-16 21:55:54 +0300

    Update A.py

--- A.py
+++ A.py
@@ -44,29 +44,29 @@ for i in range(sweller_n):
 # координаты по x и по y у каждого объекта в своем массиве(по x и по y соответственно)
 for i in range(sweller_n):
     if swx[i] == math.floor(swx[i]) and swy[i] == math.floor(swy[i]):
-        print("minimal distance for sweller number" + " " + str(i + 1) + ":" + str(m_distance1(swx[i], swy[i])))
+        print("minimal distance for dweller number" + " " + str(i + 1) + ":" + str(m_distance1(swx[i], swy[i])))
         print("the school is:")
         m_distance2(swx[i],swy[i])
     if swx[i] == math.floor(swx[i]) and swy[i] != math.floor(swy[i]):
         min1 = m_distance1(swx[i], math.floor(swy[i]))
         min2 = m_distance1(swx[i], math.floor(swy[i] + 1))
         if min1 + abs(swy[i] - math.floor(swy[i])) < min2 + abs(math.floor(swy[i] + 1) - swy[i]):
-            print("minimal distance for sweller number" + " " + str(i + 1) +  ": " + str(min1 + swy[i] - math.floor(swy[i])))
+            print("minimal distance for dweller number" + " " + str(i + 1) +  ": " + str(min1 + swy[i] - math.floor(swy[i])))
             print("the school is:")
             m_distance2(swx[i], math.floor(swy[i]))
         else:
-            print("minimal distance for sweller number" + " " + str(i + 1) + ": " + str(min2 - swy[i] + math.floor(swy[i] + 1)))
+            print("minimal distance for dweller number" + " " + str(i + 1) + ": " + str(min2 - swy[i] + math.floor(swy[i] + 1)))
             print("the school is:")
             m_distance2(swx[i], math.floor(swy[i] + 1))
     if swx[i] != math.floor(swx[i]) and swy[i] == math.floor(swy[i]):
         min1 = m_distance1(math.floor(swx[i]),swy[i])
         min2 = m_distance1(math.floor(swx[i] + 1),swy[i])
         if min1 + abs(swx[i] - math.floor(swx[i])) < min2 + abs(math.floor(swx[i] + 1) - swx[i]):
-            print("minimal distance for sweller number" + " " + str(i + 1) + ": " + str(min1 + abs(swx[i] - math.floor(swx[i]))))
+            print("minimal distance for dweller number" + " " + str(i + 1) + ": " + str(min1 + abs(swx[i] - math.floor(swx[i]))))
             print("the school is:")
             m_distance2(math.floor(swx[i]),swy[i])
         else:
-            print("minimal distance for sweller number" + " " + str(i + 1) +  ": " + str(min2 - swx[i] + math.floor(swx[i] + 1)))
+            print("minimal distance for dweller number" + " " + str(i + 1) +  ": " + str(min2 - swx[i] + math.floor(swx[i] + 1)))
             m_distance2(math.floor(swx[i] + 1),swy[i])
     if swx[i] != math.floor(swx[i]) and swy[i] != math.floor(swy[i]):
         min1 = m_distance1(math.floor(swx[i]),math.floor(swy[i])) + swx[i] - math.floor(swx[i]) + swy[i] - math.floor(swy[i])
@@ -75,19 +75,19 @@ for i in range(sweller_n):
         min4 = m_distance1(math.floor(swx[i]), math.floor(swy[i] + 1)) + swx[i] - math.floor(swx[i]) - swy[i] + math.floor(swy[i] + 1)
         min5 = min(min1,min2,min3,min4)
         if min5 == min1:
-            print("minimal distance for sweller number" + " " + str(i + 1) + ": " + str(min1))
+            print("minimal distance for dweller number" + " " + str(i + 1) + ": " + str(min1))
             print("the school is:")
             m_distance2(math.floor(swx[i]), math.floor(swy[i]))
         if min5 == min2 and min5 != min1:
-            print("minimal distance for sweller number" + " " + str(i + 1) + ": " + str(min2))
+            print("minimal distance for dweller number" + " " + str(i + 1) + ": " + str(min2))
             print("the school is:")
             m_distance2(math.floor(swx[i] + 1),math.floor(swy[i]))
         if min5 == min3 and min5 != min1 and min5 != min2:
-            print("minimal distance for sweller number" + " " + str(i + 1) + ": " + str(min3))
+            print("minimal distance for dweller number" + " " + str(i + 1) + ": " + str(min3))
             print("the school is:")
             m_distance2(math.floor(swx[i] + 1), math.floor(swy[i] + 1))
         if min5 == min4 and min5 != min1 and min5 != min2 and min5 != min3:
-            print("minimal distance for sweller number" + " " + str(i + 1) + ": " + str(min4))
+            print("minimal distance for dweller number" + " " + str(i + 1) + ": " + str(min4))
             print("the school is:")
             m_distance2(math.floor(swx[i]), math.floor(swy[i] + 1))
         min1 = 123123
